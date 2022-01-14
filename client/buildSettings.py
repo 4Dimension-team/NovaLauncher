1806e5fe5124f0a16396d6f841b1c2f3a23ec596
NullPointerException <61092992+cd-con@users.noreply.github.com>
2022-01-14 18:35:43 +0300

    памятка

--- client/buildSettings.py
+++ client/buildSettings.py
@@ -3,6 +3,7 @@ CLIENT_VERSION = 0
 CLIENT_NAME = 'Nova Launcher'
 DEBUG = True
 
+# Указан случайный IP; даже не пытайся дудосить, всё равно ъуй соснёшь
 SERVER_IP = 'http://92.63.96.235'
 FILE_PATH = 'static/client_1.12.2.zip'
 
